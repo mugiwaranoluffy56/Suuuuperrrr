9c2c2c853ea9df940c51ad6ca0f85644eb820974
mugiwaranoluffy56 <kushaln500@gmail.com>
2025-03-07 15:16:41 +0530

    Update HiAnime.kt

--- src/HiAnime.kt
+++ src/HiAnime.kt
@@ -1,116 +1,108 @@
-package eu.kanade.tachiyomi.extension.en.hianime
+package com.hianime.extension
 
+import eu.kanade.tachiyomi.animesource.AnimeSource
+import eu.kanade.tachiyomi.animesource.model.*
+import eu.kanade.tachiyomi.animesource.online.ParsedAnimeHttpSource
 import eu.kanade.tachiyomi.network.GET
-import eu.kanade.tachiyomi.source.model.FilterList
-import eu.kanade.tachiyomi.source.model.Page
-import eu.kanade.tachiyomi.source.model.SEpisode
-import eu.kanade.tachiyomi.source.model.SAnime
-import eu.kanade.tachiyomi.source.online.HttpSource
+import eu.kanade.tachiyomi.network.asObservable
+import eu.kanade.tachiyomi.util.asJsoup
 import okhttp3.Request
 import okhttp3.Response
-import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+import rx.Observable
 
-class HiAnime : HttpSource() {
+class HiAnime : ParsedAnimeHttpSource() {
+
     override val name = "HiAnime"
-    override val baseUrl = "https://www.hianime.to"
+    override val baseUrl = "https://hianime.to"
     override val lang = "en"
     override val supportsLatest = true
 
+    // Fetch popular anime
     override fun popularAnimeRequest(page: Int): Request {
         return GET("$baseUrl/popular?page=$page", headers)
     }
 
-    override fun popularAnimeParse(response: Response): AnimesPage {
-        val document = Jsoup.parse(response.body.string())
-        val animes = document.select("div.anime-list > div.anime-item").map {
-            SAnime.create().apply {
-                title = it.select("a.title").text()
-                url = it.select("a").attr("href")
-                thumbnail_url = it.select("img").attr("src")
-            }
+    override fun popularAnimeParse(response: Response): AnimePage {
+        val document = response.asJsoup()
+        val animeList = document.select("div.anime-list div.anime-item").map { element ->
+            popularAnimeFromElement(element)
         }
-        return AnimesPage(animes, hasNextPage = true)
+        val hasNextPage = document.select("a.next-page").isNotEmpty()
+        return AnimePage(animeList, hasNextPage)
     }
 
-    override fun animeDetailsRequest(anime: SAnime): Request {
-        return GET(baseUrl + anime.url, headers)
+    private fun popularAnimeFromElement(element: Element): SAnime {
+        return SAnime.create().apply {
+            title = element.select("h3.title").text()
+            thumbnail_url = element.select("img.thumbnail").attr("abs:src")
+            setUrlWithoutDomain(element.select("a").attr("href"))
+        }
     }
 
-    override fun animeDetailsParse(response: Response): SAnime {
-        val document = Jsoup.parse(response.body.string())
+    // Fetch anime details
+    override fun animeDetailsParse(document: Document): SAnime {
         return SAnime.create().apply {
             title = document.select("h1.title").text()
-            author = document.select("span.author").text()
+            thumbnail_url = document.select("img.thumbnail").attr("abs:src")
             description = document.select("div.description").text()
-            genre = document.select("span.genre").joinToString { it.text() }
-            status = SAnime.UNKNOWN
-            thumbnail_url = document.select("img.thumbnail").attr("src")
         }
     }
 
-    override fun episodeListRequest(anime: SAnime): Request {
-        return GET(baseUrl + anime.url, headers)
-    }
-
+    // Fetch episode list
     override fun episodeListParse(response: Response): List<SEpisode> {
-        val document = Jsoup.parse(response.body.string())
-        return document.select("div.episode-row").map {
+        val document = response.asJsoup()
+        return document.select("div.episode-list div.episode-item").map { element ->
             SEpisode.create().apply {
-                name = it.select("a.title").text()
-                url = it.select("a").attr("href")
-                date_upload = it.select("span.date").text().toLong()
+                name = element.select("h4.title").text()
+                episode_number = element.select("span.episode-number").text().toFloatOrNull() ?: 0f
+                date_upload = System.currentTimeMillis()
+                setUrlWithoutDomain(element.select("a").attr("href"))
             }
         }
     }
 
-    override fun videoListRequest(episode: SEpisode): Request {
-        return GET(baseUrl + episode.url, headers)
-    }
-
-    override fun videoListParse(response: Response): List<Page> {
-        val document = Jsoup.parse(response.body.string())
-        return document.select("div.video-player").mapIndexed { index, element ->
-            Page(index, "", element.select("source").attr("src"))
+    // Fetch video URLs
+    override fun videoListParse(response: Response): List<Video> {
+        val document = response.asJsoup()
+        return document.select("div.video-source").map { element ->
+            Video(element.select("source").attr("src"), element.select("source").attr("type"), element.select("source").attr("src"))
         }
     }
 
-    override fun imageUrlParse(response: Response): String {
-        throw UnsupportedOperationException("Not used.")
-    }
-
-    override fun getFilterList(): FilterList {
-        return FilterList()
-    }
-
-    override fun searchAnimeRequest(page: Int, query: String, filters: FilterList): Request {
+    // Other required methods (search, latest updates, etc.)
+    override fun searchAnimeRequest(page: Int, query: String, filters: AnimeFilterList): Request {
         return GET("$baseUrl/search?q=$query&page=$page", headers)
     }
 
-    override fun searchAnimeParse(response: Response): AnimesPage {
-        val document = Jsoup.parse(response.body.string())
-        val animes = document.select("div.anime-list > div.anime-item").map {
-            SAnime.create().apply {
-                title = it.select("a.title").text()
-                url = it.select("a").attr("href")
-                thumbnail_url = it.select("img").attr("src")
-            }
+    override fun searchAnimeParse(response: Response): AnimePage {
+        val document = response.asJsoup()
+        val animeList = document.select("div.search-results div.anime-item").map { element ->
+            searchAnimeFromElement(element)
         }
-        return AnimesPage(animes, hasNextPage = true)
+        val hasNextPage = document.select("a.next-page").isNotEmpty()
+        return AnimePage(animeList, hasNextPage)
+    }
+
+    private fun searchAnimeFromElement(element: Element): SAnime {
+        return SAnime.create().apply {
+            title = element.select("h3.title").text()
+            thumbnail_url = element.select("img.thumbnail").attr("abs:src")
+            setUrlWithoutDomain(element.select("a").attr("href"))
+        }
     }
 
     override fun latestUpdatesRequest(page: Int): Request {
         return GET("$baseUrl/latest?page=$page", headers)
     }
 
-    override fun latestUpdatesParse(response: Response): AnimesPage {
-        val document = Jsoup.parse(response.body.string())
-        val animes = document.select("div.anime-list > div.anime-item").map {
-            SAnime.create().apply {
-                title = it.select("a.title").text()
-                url = it.select("a").attr("href")
-                thumbnail_url = it.select("img").attr("src")
-            }
+    override fun latestUpdatesParse(response: Response): AnimePage {
+        val document = response.asJsoup()
+        val animeList = document.select("div.latest-updates div.anime-item").map { element ->
+            popularAnimeFromElement(element)
         }
-        return AnimesPage(animes, hasNextPage = true)
+        val hasNextPage = document.select("a.next-page").isNotEmpty()
+        return AnimePage(animeList, hasNextPage)
     }
 }
